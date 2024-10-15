7c6f6e017e8cddbb102bcaea81dd7e7d1922ed42
Sparc <sparcjarry@gmail.com>
2024-10-15 17:23:39 -0600

    Update HeapSort.c

--- Sortings/HeapSort.c
+++ Sortings/HeapSort.c
@@ -13,7 +13,7 @@ void HeapSort(int* p, uint n)
 	// Heap sort
 	for (int i = n; i; i--) {
 		SWAP(1, i);
-		for (uint j = 1u; j < (i - 1) >> 1 | 1;) {
+		for (uint j = 1u; j < ((i - 1) >> 1) + 1;) {
 			uint max_i = j << 1;
 			if (q[max_i | 1] > q[max_i] && max_i | 1 != i)  max_i++;
 			if (q[max_i] > q[j])  SWAP(j, max_i);
