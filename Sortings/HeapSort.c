2169253948f8d575107fe0194b6d4c4d15978d4e
Sparc <sparcjarry@gmail.com>
2024-10-15 17:21:51 -0600

    Update HeapSort.c

--- Sortings/HeapSort.c
+++ Sortings/HeapSort.c
@@ -8,14 +8,14 @@ void HeapSort(int* p, uint n)
 {
 	int* const q = p - 1;
 	// Initialization heapify
-	for (uint i = 2u; i <= n; i++)
+	for (int i = 2; i <= n; i++)
 		for (uint j = i; q[j >> 1] < q[j] && j != 1u; j >>= 1)  SWAP(j >> 1, j);
 	// Heap sort
-	for (uint i = n; i; i--) {
+	for (int i = n; i; i--) {
 		SWAP(1, i);
-		for (uint j = 1u; j < ((i - 1) >> 1) + 1;) {
+		for (uint j = 1u; j < (i - 1) >> 1 | 1;) {
 			uint max_i = j << 1;
-			if (q[max_i + 1] > q[max_i] && max_i + 1 != i)  max_i++;
+			if (q[max_i | 1] > q[max_i] && max_i | 1 != i)  max_i++;
 			if (q[max_i] > q[j])  SWAP(j, max_i);
 			j = max_i;
 		}
